d6e74cac266aea671f98eff7e7e26b1ac7817d4b
ycycwx <yytcjcy@gmail.com>
2024-10-28 22:20:48 +0800

    chore: upgrade nextjs 15

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import createMDX from '@next/mdx';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
+import type {NextConfig} from 'next';
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+const nextConfig: NextConfig = {
     output: 'export',
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
     reactStrictMode: true,
-    swcMinify: true,
 };
 
 export default createMDX({
