98b629a3d43995ef86973ee0a00e8e52b64608ed
ycycwx <yytcjcy@gmail.com>
2025-10-26 13:57:27 +0800

    chore: fix eslint issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,7 @@
-import path from 'node:path';
-import {fileURLToPath} from 'node:url';
-import {FlatCompat} from '@eslint/eslintrc';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 import config from '@yotsubacy/config/eslint';
 import {globalIgnores} from 'eslint/config';
-
-const compat = new FlatCompat({
-    baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
-});
 
 export default config(
     ['node', 'browser', 'react', 'typescript'],
@@ -27,11 +22,12 @@ export default config(
             },
         },
     },
+    ...nextVitals,
+    ...nextTs,
     globalIgnores([
         'out',
         '.*',
         '**/*.d.ts',
         'styled-system/**/*.mjs',
-    ]),
-    ...compat.extends('next/core-web-vitals')
+    ])
 );
